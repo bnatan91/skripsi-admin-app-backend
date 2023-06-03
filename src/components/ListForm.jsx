54fdc7cf2d29c9accc21d49ebc418226066745a5
billy <billynatanael91@gmail.com>
2023-06-03 15:52:32 +0700

    update list form

--- src/components/ListForm.jsx
+++ src/components/ListForm.jsx
@@ -1,18 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import Form from './Form';
 
-const normalizeValue = (data) => {
-  console.log(data);
+const matrix = (data) => {
+  // console.log(data);
+  const valueArr = [];
+  const checkedArr = [];
+  data.map((subject, index) => {
+    console.log(subject.value);
+    valueArr.push({
+      id: index + 1,
+      value: subject.value,
+    });
+    checkedArr.push({
+      id: index + 1,
+      checked: subject.checked,
+    });
+  });
+  normalizeMatrix(data, valueArr, checkedArr);
+};
+
+const normalizeMatrix = (data, valueArr, checkedArr) => {
+  // console.log(data, valueArr, checkedArr);
+  const sortValue = valueArr.sort((a, b) => b.value - a.value);
+  const sortChecked = checkedArr.sort((a, b) => b.checked - a.checked);
+  // console.log(sortValue, sortChecked);
+  calculationMatrix(data, sortValue, sortChecked);
+};
+
+const calculationMatrix = (data, value, checked) => {
+  console.log(data, value, checked);
 };
 
 function ListForm(props) {
-  const [inputSubject, setInputSubject] = useState('');
+  const [inputSubject, setInputSubject] = useState([]);
   const submitHandler = (data) => {
     setInputSubject(data);
   };
 
+  matrix(inputSubject);
   const submitValue = () => {
-    normalizeValue(inputSubject);
     props.onSubmit(inputSubject);
   };
 
@@ -21,7 +47,6 @@ function ListForm(props) {
     props.onModal(data);
   };
   //   console.log(inputSubject);
-  submitValue();
   return (
     <div>
       <Form onSubmit={submitHandler} onModal={modalHandler} />
